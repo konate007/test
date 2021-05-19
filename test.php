e7ae5a941bded3e7c36219a8517bf06ec94b5904
KONATE <monsieurkonate007@gmail.com>
2021-05-19 13:49:11 +0200

    first-commit

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Laborum possimus explicabo dolorem officia libero minus modi
+        a animi, ullam laudantium harum, incidunt sapiente ipsum eveniet!
+        Voluptates ipsam neque consequuntur maxime.
+</p>
+</body>
+</html>
